4b94f9b7bf1e0c8c09e8006c3a24ea288882f94a
Andrzej Wojciechowski <divart@divart.pl>
2017-04-19 10:04:55 +0200

    add site-info

--- components/footer/site-info.php
+++ components/footer/site-info.php
@@ -1,5 +1,23 @@
 <div class="site-info">
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'amini' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'amini' ), 'WordPress' ); ?></a>
-	<span class="sep"> | </span>
-	<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'amini' ), 'aMini', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+Copyright &copy;&nbsp;<?php echo date("Y"); ?>
+
+<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+
+<span class="sep"> &#8226; </span>
+
+<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'amini' ) ); ?>"><?php printf( esc_html__( 'Powered by %s', 'amini' ), 'WordPress' ); ?></a>
+
+<span class="sep"> &#8226; </span>
+
+<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'amini' ), 'aMini', '<a href="http://divart.pl/" rel="designer">DIVart</a>' ); ?>
+
+<span class="sep"> &#8226; </span>
+
+<?php 
+if ( ! is_user_logged_in() ) {
+	echo '<a href="' . esc_url( wp_login_url()) . '">'.__( 'Log in', 'amini' ).'</a>';
+} else {
+	echo '<a href="' . esc_url( wp_logout_url()) . '">'.__( 'Log out', 'amini' ).'</a>';
+	}
+?>
 </div><!-- .site-info -->
