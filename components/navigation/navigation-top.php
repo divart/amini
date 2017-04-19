112a3c98b2491eb6212e1e28de439438cbfc7f0d
Andrzej Wojciechowski <divart@divart.pl>
2017-04-19 13:44:00 +0200

    add menu style

--- components/navigation/navigation-top.php
+++ components/navigation/navigation-top.php
@@ -1,4 +1,4 @@
-<nav id="site-navigation" class="main-navigation col-6 col-md-8" role="navigation">
+<nav id="site-navigation" class="main-navigation col-4 col-md-8" role="navigation">
 	<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'amini' ); ?></button>
 	<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'top-menu' ) ); ?>
 </nav>
